1a542b3b842c5ae6fea0ff0627e05bdd64458385
Lukas <crazyluke11@users.noreply.github.com>
2023-12-11 21:14:12 +0100

    i dont know

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,4 @@
+
 # Edet this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -104,7 +105,7 @@
      qpwgraph
      pavucontrol
      bluez
-     nitrogen
+     swww
      dolphin
 
      #picom
