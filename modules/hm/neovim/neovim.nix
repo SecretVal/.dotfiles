14bd0f11d9648cc7d67901cc8e0459b95c50dd9c
Lukas <crazyluke11@users.noreply.github.com>
2023-12-12 18:11:48 +0100

    hypr and vim

--- modules/hm/neovim/neovim.nix
+++ modules/hm/neovim/neovim.nix
@@ -60,7 +60,11 @@
       lsp = {
         enable = true;
         servers = {
-          rust-analyzer.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installCargo = false;
+            installRustc = false;
+          };
           nixd.enable = true;
           html.enable = true;
           jsonls.enable = true;
