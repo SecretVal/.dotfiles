69153e5c6e8da40b7d146010c5f5ae6696f3872d
secretval <108518296+secretval@users.noreply.github.com>
2024-01-02 14:59:35 +0100

    tried to fix

--- modules/hm/hyprland/hyprland.nix
+++ modules/hm/hyprland/hyprland.nix
@@ -36,15 +36,9 @@ exec-once=wl-copy
 # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
 input {
   kb_layout = de
-  kb_variant =
-  kb_model =
-  kb_options =
-  kb_rules =
-  follow_mouse = 2
-  touchpad {
-    natural_scroll = no
-  }
+  follow_mouse = 1
   force_no_accel = true
+  accel_profile = flat
   sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
 }
 general {
