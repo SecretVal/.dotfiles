90aa13bfbc6a2fb0ca646d69f94ac3d6d9340513
secretval <108518296+secretval@users.noreply.github.com>
2024-01-05 20:51:19 +0100

    vim

--- modules/hm/neovim/opt.nix
+++ modules/hm/neovim/opt.nix
@@ -13,7 +13,6 @@
 
       number = true;
       relativenumber = true;
-
       tabstop = 4;
       softtabstop = 4;
       shiftwidth = 4;
@@ -29,16 +28,16 @@
       incsearch = true;
 
       termguicolors = true;
-
       scrolloff = 8;
       signcolumn = "yes";
 
       updatetime = 50;
+       
 
       colorcolumn = "80";
 
       list = true;
-      listchars = "eol:↲";
-    };
+      listchars = "eol:↲,multispace:⠀·";
+    } ;
   };
 }
