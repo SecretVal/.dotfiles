b9402b3e4e04a783873cf4e300f4012fe30ec34d
secretval <108518296+secretval@users.noreply.github.com>
2024-01-11 19:35:09 +0100

    look desc
    I fucking deleted a file but forgot to commit and had to redo it 😠

--- modules/hm/neovim/plugins/default.nix
+++ modules/hm/neovim/plugins/default.nix
@@ -1,7 +1,8 @@
 {config, lib,...}:{
-  modules = [
+  imports = [
     ./color.nix
     ./plugins.nix
+    ./treesitter.nix
     ./telescope.nix
   ];
 }
--- modules/hm/neovim/plugins/treesitter.nix
+++ modules/hm/neovim/plugins/treesitter.nix
@@ -3,9 +3,36 @@
   programs.nixvim = {
     plugins = {
       treesitter = {
-
+        enable = true;
+        ensureInstalled = "all";
+        nixvimInjections = true;
       };
-      treesitter-context.enable = true {
+      treesitter-context.enable = true;
+      treesitter-textobjects = {
+        enable = true;
+        select = {
+          enable = true;
+          keymaps = {
+            "af"= "@function.outer";
+            "if" = "@function.inner";
+
+            "ab"= "@block.outer";
+            "ib"= "@block.inner";
+
+            "aif"= "@conditional.outer";
+            "iif"= "@conditional.inner";
+
+            "al"= "@loop.outer";
+            "il"= "@loop.inner";
+
+            "at"= "@comment.outer";
+            "it"= "@comment.inner";
+
+            "s"= "@statement.outer";
+            "as" = "@scopename.inner";
+          };
+        };
+      };
     };
   };
 }
